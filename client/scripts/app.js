9a2277eb87eefb16c4ffd109d490913d27786159
theesweeney <bsweeney930@gmail.com>
2015-12-07 15:14:26 -0800

    Completed sending & fetching requirements for chatterbox application

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,18 @@
-// YOUR CODE HERE:
+var app = {
+  server: "https://api.parse.com/1/classes/chatterbox",
+  init: function(){},
+  send: function(message){
+    $.ajax({
+      type: "POST",
+      url: "https://api.parse.com/1/classes/chatterbox",
+      data: JSON.stringify(message),
+      // dataType: "object"
+    });
+  },
+  fetch: function(){
+    $.ajax({
+      url: "https://api.parse.com/1/classes/chatterbox",
+      type: "GET"
+    });
+  }
+};
