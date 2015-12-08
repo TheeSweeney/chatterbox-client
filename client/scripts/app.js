db6c385cdf8f29fa3c87cdbd2f71f70d26cf5dce
theesweeney <bsweeney930@gmail.com>
2015-12-07 20:03:21 -0800

    Styled & incorporated active served chatroom into application

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,16 @@
+function escapeHtml(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+};
+
 var app = {
   server: "https://api.parse.com/1/classes/chatterbox",
-  init: function(){},
+  init: function(){
+    // app.fetch();
+    //commented fetch out to test idea of only loading 
+    //msgs on demand a la whatsapp
+  },
   send: function(message){
     $.ajax({
       type: "POST",
@@ -11,15 +21,23 @@ var app = {
   fetch: function(){
     $.ajax({
       url: "https://api.parse.com/1/classes/chatterbox",
-      type: "GET"
+      type: "GET",
+      success: function(data){
+        for(var i = 0; i < 20; i++){
+          var cur = data.results[i];
+          app.addMessage({username: cur.username, text: cur.text, roomname: cur.roomname});
+        }
+        
+      }
     });
   },
   clearMessages: function(){
     $('#chats').children().remove();
   },
   addMessage: function(message){
+
     var username = $('<a/>', {
-      text: message.username, 
+      text: message.username || '< no username set >', 
       href:'#', 
       class: 'username', 
       click: function(){
@@ -27,16 +45,26 @@ var app = {
       } 
     });
 
+    var roomname = $('<a/>', {
+      text: message.roomname || '< no room identified >', 
+      href:'#', 
+      class: 'roomname', 
+      // click: function(){
+      //   app.addFriend();
+      // } 
+    });
+
     var post = $('<p/>', {
-      text: message.text, 
+      text: message.text || '< no text entered >', 
       class:'post'
     });
 
-    var msg = $('<div/>');
+    var msg = $('<li/>');
 
     username.appendTo(msg);
+    roomname.appendTo(msg);
     post.appendTo(msg);
-    msg.appendTo('#chats');
+    msg.prependTo('#chats');
   },
   addRoom: function(room){
     $('<div/>', {
@@ -45,10 +73,15 @@ var app = {
   },
   addFriend: function(){},
   handleSubmit: function(){
-    var usr = "username";
+    console.log("calling");
+    var usr = window.location.search.slice(10).replace(/%20/g, ' ');
     var rm = "roomname";
     var post = $('#message').val();
     console.log(post);
-    app.addMessage({username: usr, text: post, roomname: rm});
+    app.send(
+      {username: usr, text: post, roomname: rm}
+    );
+    app.fetch();
   }
 };
+
